757f0a2ff1483d50dd5a25b58bc0624290bfa7c5
eh7 <eduardonadolny1@gmail.com>
2025-11-26 20:57:36 -0300

    arrumando

--- README.md
+++
@@ -1,7 +0,0 @@
-Comecei criando um repositorio no GitHub chamado devops-project
-
-Após isso, criei uma pasta dentro do VSCode chamado devops-project com duas pastas dentro, chamadas frontend e backend. Backend para guardar a API em python/flask e arquivo de requirements, inicialmente.
-
-Então, para testar se a API esta funcionando corretamente, criei um ambiente virtual pelo terminal, instalei as dependencias e executei o servidor localmente. Funcionando corretamente.
-
-Ainda na pasta backend, criei um Dockerfile. No terminal, fiz o build do Dockerfile através do comando [docker build -t(tag) backend-flask:0.1(nome da imagem) .]. E então com o comando [docker run -d(detached) -p(mapear a porta) 5000:5000(porta) backend-flask:0.1(imagem)], rodei a imagem.
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,18 +1,18 @@
-terraform { 
+terraform {
   required_providers {
     aws = { # define o provedor = AWS
       source  = "hashicorp/aws"
       version = "~> 5.0" # versao recente
     }
-    
-    kubernetes = {     # provedor para o EKS
+
+    kubernetes = { # provedor para o EKS
       source  = "hashicorp/kubernetes"
       version = "~> 2.23"
     }
   }
 }
 
-provider "aws" { # configurando o provider
+provider "aws" {       # configurando o provider
   region = "us-east-1" # regiao definida
 }
 
@@ -20,14 +20,14 @@ module "vpc" { # cria uma vpc usando um modulo pronto da aws
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1" # versao estavel
 
-  name = "eks-vpc" # nome da vpc
+  name = "eks-vpc"     # nome da vpc
   cidr = "10.0.0.0/16" # bloco de ip privado da vpc
 
   azs             = ["${var.region}a", "${var.region}b"] # az = availability zones, estamos usando 2 delas
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]       # as zonas estao guardadas em variaveis, encontradas na pasta variables.tf
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true # permissao para que sub-redes privadas acessem a internet
+  enable_nat_gateway   = true # permissao para que sub-redes privadas acessem a internet
   enable_dns_hostnames = true
 
   # tags para o eks saber que pode usar essa vpc
@@ -50,10 +50,10 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = { # configuracao dos nodes
-    main_nodes = { # quantidade de nodes
-      min_size     = 1 # minimo de 1 servidor
-      max_size     = 3 # caso tenham muitos inputs, chegara ate 3 servidores
-      desired_size = 2 # tentar manter 2 servidores o tempo todo
+    main_nodes = {            # quantidade de nodes
+      min_size     = 1        # minimo de 1 servidor
+      max_size     = 3        # caso tenham muitos inputs, chegara ate 3 servidores
+      desired_size = 2        # tentar manter 2 servidores o tempo todo
 
       instance_types = ["t2.micro"] # tipo de maquina
     }
